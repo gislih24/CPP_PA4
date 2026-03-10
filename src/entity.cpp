57c44566773af5a744ebffad3b12691a14492d0b
gislih24 <gislih24@ru.is>
2026-03-10 16:39:02 +0000

    Add Entity class definition with health management methods

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,18 +1,21 @@
+#include "stats.hpp"
+#include <string>
+
 class Entity {
-public:
-	virtual ~Entity() = default;
+  public:
+    virtual ~Entity() = default;
 
     std::string name;
     Stats stats;
     int hp;
 
-    virtual void take_dmg(val) {
+    virtual void take_dmg(int val) {
         hp -= val;
     }
-    
-    virtual void heal(val) {
+
+    virtual void heal(int val) {
         if ((hp + val) > stats.max_hp) {
-            hp = max_hp;
+            hp = stats.max_hp;
         } else if (hp >= 0) {
             return;
         } else {
