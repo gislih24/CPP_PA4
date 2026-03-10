fa90c6478a3d173e21eda783cc80227211fdbfd0
gislih24 <gislih24@ru.is>
2026-03-10 18:09:44 +0000

    Refactor GameState class to enhance functionality and input handling

--- src/game_state.cpp
+++
@@ -1,9 +0,0 @@
-#include "game_state.hpp"
-
-// Abstract function GameState with pure virtal functions
-class GameState {
-  public:
-    virtual void render() = 0;
-    virtual void handle_input() = 0;
-    virtual void update() = 0;
-};
--- src/game_state.hpp
+++ src/game_state.hpp
@@ -1,13 +1,45 @@
-#ifndef GAME_STATE_HPP
-#define GAME_STATE_HPP
+#pragma once
+
+#include <string_view>
+
+class Game;
 
 class GameState {
   public:
     virtual ~GameState() = default;
 
-    virtual void render() = 0;
-    virtual void handle_input() = 0;
-    virtual void update() = 0;
-};
+    virtual void on_enter(Game& game) = 0;
+    virtual void render(const Game& game) const = 0;
+    virtual void handle_input(Game& game, std::string_view input) = 0;
 
-#endif // GAME_STATE_HPP
+  protected:
+    /**
+     * @brief Normalizes the input by trimming leading and trailing whitespace
+     * and converting all characters to lowercase.
+     * @param input The input string to normalize.
+     * @return The normalized string.
+     */
+    std::string normalize_input(std::string_view input) {
+        std::size_t start = 0;
+        while (start < input.size() &&
+               std::isspace(static_cast<unsigned char>(input[start]))) {
+            ++start;
+        }
+
+        std::size_t end = input.size();
+        while (end > start &&
+               std::isspace(static_cast<unsigned char>(input[end - 1]))) {
+            --end;
+        }
+
+        std::string result;
+        result.reserve(end - start);
+
+        for (std::size_t i = start; i < end; ++i) {
+            result.push_back(static_cast<char>(
+                std::tolower(static_cast<unsigned char>(input[i]))));
+        }
+
+        return result;
+    }
+};
