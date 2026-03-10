d709d2a96e2d73b50e55ae694d1aa82410e60b00
gislih24 <gislih24@ru.is>
2026-03-10 18:10:29 +0000

    Refactor `Game` class to implement state management and enhance game loop functionality

--- src/game.hpp
+++ src/game.hpp
@@ -1,12 +1,37 @@
-#ifndef GAME_HPP
-#define GAME_HPP
+#pragma once
 
-#include "game_state.hpp"
 #include <memory>
+
+#include "world.hpp"
+
+class GameState;
 
 class Game {
   public:
-    std::unique_ptr<GameState> state;
-};
+    explicit Game(std::unique_ptr<GameState> initial_state);
 
-#endif // GAME_HPP
+    void run();
+
+    void request_state_change(std::unique_ptr<GameState> next_state);
+    void quit() noexcept;
+
+    bool is_running() const noexcept {
+        return game_is_running_;
+    }
+
+    World& get_world() noexcept {
+        return world_;
+    }
+
+    const World& get_world() const noexcept {
+        return world_;
+    }
+
+  private:
+    void apply_pending_state_change();
+
+    World world_{};
+    std::unique_ptr<GameState> current_state_;
+    std::unique_ptr<GameState> pending_state_;
+    bool game_is_running_{true};
+};
