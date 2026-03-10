468558510394051c6341b9898d53075a1c29d6d2
gislih24 <gislih24@ru.is>
2026-03-10 18:10:17 +0000

    Implement `MainMenuState` class with input handling and rendering functionality

---
+++ src/main_menu_state.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+#include "game_state.hpp"
+
+class MainMenuState final : public GameState {
+  public:
+    void on_enter(Game& game) override;
+    void render(const Game& game) const override;
+    void handle_input(Game& game, std::string_view input) override;
+
+  private:
+    std::string message_;
+};
