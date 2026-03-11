6a5c0db824b1e871977298fdadb6e465f8e16f78
gislih24 <gislih24@ru.is>
2026-03-11 13:44:22 +0000

    Add initial implementation of new `BattleState` system

--- src/battle_state.cpp
+++ src/battle_state.cpp
@@ -3,8 +3,9 @@
 #include "game_state.hpp"
 #include "player_character.hpp"
 #include <iostream>
-#include <print>
+// #include <print>
 #include <string>
+#include <vector>
 
 BattleState::BattleState() {
     pc.stats.attack = 2;
@@ -18,55 +19,70 @@ BattleState::BattleState() {
     enemy.hp = enemy.stats.max_hp;
 }
 
-void BattleState::render() override {
-    std::print("Woe! A fiend is upon ye!\n");
+void BattleState::on_enter(Game& game) {
+    combat_log_.push_back("Woe, a fiend is upon ye!");
+}
 
-    while (in_battle && (pc.hp > 0) && (enemy.hp > 0)) {
+void BattleState::render(const Game& game) const {
+    // For each of the message vectors
+    for (const auto* message_vector :
+         {&combat_log_, &status_display_, &action_menu_}) {
+        // For each message in the current message vector
+        for (const auto& message : *message_vector) {
+            std::cout << message << '\n'; // Print it
+        }
+    }
+}
+
+void BattleState::handle_input(Game& game, std::string_view input) {
+    clear_message_vectors();
+    if (in_battle && (pc.hp > 0) && (enemy.hp > 0)) {
         // player turn
-        std::print("Choose an action:\n1. attack\n0. flee\n");
-        std::getline(std::cin, line);
-        int choice = std::stoi(line);
-        switch (choice) {
-        case 1:
+        action_menu_.push_back("Choose an action:\n1. attack\n0. flee\n");
+        if (input == "1") {
             // attack
             damage_dealt = pc.attack(enemy);
             if (damage_dealt > 0) {
-                std::print("You hit the enemy and dealt {} damage\n",
-                           damage_dealt);
+
+                action_menu_.push_back(
+                    "Choose an action:\n1. attack\n0. flee\n");
+                std::cout << "You hit the enemy and dealt {} damage\n"
+                          << damage_dealt;
             }
             if (!enemy.is_alive()) {
                 in_battle = false;
             }
-            break;
-        case 0:
+        } else if (input == "2") {
             // flee
-            std::print("You flee... a coward's choice\n");
+            std::cout << "You flee... a coward's choice\n";
             in_battle = false;
-            break;
-        default:
-            std::print("Invalid choice\n");
+        }
+    default:
+            std::cout <<"Invalid choice\n");
             continue;
-        }
+    }
 
-        if (!in_battle || !enemy.is_alive()) {
-            break;
-        }
+    if (!in_battle || !enemy.is_alive()) {
+        break;
+    }
 
-        // enemy's turn
-        damage_dealt = enemy.attack(pc);
-        std::print("You were hit for {} damage\n", damage_dealt);
+    // enemy's turn
+    damage_dealt = enemy.attack(pc);
+        std::cout <<"You were hit for {} damage\n", damage_dealt);
         if (!pc.is_alive()) {
             in_battle = false;
             break;
         }
-    }
-    if (!pc.is_alive()) {
-        std::print("You have died... shameful display!\n");
-    } else if (!enemy.is_alive()) {
-        std::print("You emerge victorious!\n");
-    }
+}
+if (!pc.is_alive()) {
+        std::cout <<"You have died... shameful display!\n");
+} else if (!enemy.is_alive()) {
+        std::cout <<"You emerge victorious!\n");
+}
 }
 
-void BattleState::handle_input() override {}
-
-void BattleState::update() override {}
+void BattleState::clear_message_vectors() {
+    combat_log_.clear();
+    status_display_.clear();
+    action_menu_.clear();
+}
