1a29f2d22c363a24bc0e1a05d2801d70af14f1e2
gislih24 <gislih24@ru.is>
2026-03-11 10:25:41 +0000

    Use clang formatter on code

--- src/battle_state.cpp
+++ src/battle_state.cpp
@@ -1,12 +1,12 @@
-#include "game_state.cpp"
 #include "enemy.cpp"
+#include "game_state.cpp"
 #include "player_character.cpp"
 #include <iostream>
 #include <print>
 #include <string>
 
 class BattleState : public GameState {
-public:
+  public:
     PlayerCharacter pc = PlayerCharacter{};
     Enemy enemy = Enemy{};
 
@@ -21,63 +21,61 @@ public:
         enemy.stats.max_hp = 10;
         enemy.hp = enemy.stats.max_hp;
     }
-    
+
     std::string line;
     bool in_battle = true;
     int damage_dealt;
 
-	void render() override {
-    std::print("Woe! A fiend is upon ye!\n");
+    void render() override {
+        std::print("Woe! A fiend is upon ye!\n");
 
         while (in_battle && (pc.hp > 0) && (enemy.hp > 0)) {
-            //player turn
+            // player turn
             std::print("Choose an action:\n1. attack\n0. flee\n");
             std::getline(std::cin, line);
             int choice = std::stoi(line);
-                switch (choice) {
-                    case 1:
-                        //attack
-                        damage_dealt = pc.attack(enemy);
-                        if (damage_dealt > 0) {
-                            std::print("You hit the enemy and dealt {} damage\n", damage_dealt);
-                        }
-                        if (!enemy.is_alive()) {
-                            in_battle = false;
-                        }
-                        break;
-                    case 0:
-                        //flee
-                        std::print("You flee... a coward's choice\n");
-                        in_battle = false;
-                        break;
-                    default:
-                        std::print("Invalid choice\n");
-                        continue;
-                    }
-
-                if (!in_battle || !enemy.is_alive()) {
-                    break;
+            switch (choice) {
+            case 1:
+                // attack
+                damage_dealt = pc.attack(enemy);
+                if (damage_dealt > 0) {
+                    std::print("You hit the enemy and dealt {} damage\n",
+                               damage_dealt);
                 }
-
-                //enemy's turn
-                damage_dealt = enemy.attack(pc);
-                std::print("You were hit for {} damage\n", damage_dealt);
-                if (!pc.is_alive()) {
+                if (!enemy.is_alive()) {
                     in_battle = false;
-                    break;
                 }
+                break;
+            case 0:
+                // flee
+                std::print("You flee... a coward's choice\n");
+                in_battle = false;
+                break;
+            default:
+                std::print("Invalid choice\n");
+                continue;
             }
+
+            if (!in_battle || !enemy.is_alive()) {
+                break;
+            }
+
+            // enemy's turn
+            damage_dealt = enemy.attack(pc);
+            std::print("You were hit for {} damage\n", damage_dealt);
             if (!pc.is_alive()) {
-                std::print("You have died... shameful display!\n");
-            } else if (!enemy.is_alive()) {
-                std::print("You emerge victorious!\n");
+                in_battle = false;
+                break;
             }
         }
+        if (!pc.is_alive()) {
+            std::print("You have died... shameful display!\n");
+        } else if (!enemy.is_alive()) {
+            std::print("You emerge victorious!\n");
+        }
+    }
 
-	void handle_input() override {
-	}
+    void handle_input() override {}
 
-	void update() override {
-	}
+    void update() override {}
 };
-
