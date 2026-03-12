a2f9f9166306e970c2e328f8d5ccad9dffe52407
Hawkurit <haukurit@gmail.com>
2026-03-12 14:31:27 +0000

    Add character classes: Knight, Wizard, and Rogue to character_classes.cpp

---
+++ src/character_classes.cpp
@@ -0,0 +1,15 @@
+#include "player_character.cpp"
+
+class Knight : public PlayerCharacter {
+    void shield_brace() {
+        // ability that increases defence for all allies
+    }
+};
+
+class Wizard : public PlayerCharacter {
+    
+};
+
+class Rogue : public PlayerCharacter {
+
+};
