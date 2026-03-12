e25d9dfc48afc5e7001ad4c5d0ab5d2c39eaef93
gislih24 <gislih24@ru.is>
2026-03-12 15:08:12 +0000

    Implement `ExploreState` class with combat log, status display, and action menu

--- include/explore_state.hpp
+++ include/explore_state.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "game_state.hpp"
+#include <vector>
 
 class ExploreState final : public GameState {
   public:
@@ -9,5 +10,16 @@ class ExploreState final : public GameState {
     void handle_input(Game& game, std::string_view input) override;
 
   private:
-    std::string message_;
+    // Actions that occurred in combat,
+    // e.g.: "player attacks for 7 damage", "enemy attacks for 4 damage",
+    // "player has lost status effect: 2x damage"
+    std::vector<std::string> combat_log_ = {};
+    // The current status of things,
+    // e.g.: "enemy health: 7/15 HP" "player health: 9/12 HP".
+    std::vector<std::string> status_display_ = {};
+    // Actions currently available to the player,
+    // e.g.: "Choose an action: 1. Attack, 2. Inventory, 3. Flee"
+    std::vector<std::string> action_menu_ = {};
+
+    void clear_message_vectors();
 };
