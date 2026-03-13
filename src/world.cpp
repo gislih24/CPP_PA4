394d4fb1ca82480feea2c5775978f7a6cb1a51c8
gislih24 <gislih24@ru.is>
2026-03-13 10:37:39 +0000

    Refactor `World` class: `add to_index()` function, reset enemy stats, and adjust initial player stats in `reset_new_game()` method

--- src/world.cpp
+++ src/world.cpp
@@ -2,6 +2,7 @@
 #include "./include/enemy.hpp"
 #include "./include/player_character.hpp"
 #include "./include/stats.hpp"
+#include <algorithm>
 #include <format>
 #include <inttypes.h>
 #include <memory>
@@ -9,6 +10,10 @@
 namespace {
 constexpr int WORLD_HEIGHT = 5;
 constexpr int WORLD_WIDTH = 5;
+
+std::size_t to_index(int value) {
+    return static_cast<std::size_t>(value);
+}
 } // namespace
 
 World::World()
@@ -17,24 +22,29 @@ World::World()
                            std::vector<Entity*>(WORLD_WIDTH, nullptr)) {}
 
 void World::reset_new_game() {
+    defeated_enemies_ = 0;
     enemies_.clear();
     for (auto& row : overworld_occupants_) {
         std::ranges::fill(row, nullptr);
     }
 
-    auto initial_stats = Stats{8, 10, 12};
+    auto initial_stats = Stats{20, 6, 2};
     player_ =
         PlayerCharacter{"The Knight", initial_stats, initial_stats.max_hp};
     player_.set_position(0, 0);
     for (int i = 0; i < 3; i++) {
         std::string new_enemy_name = std::format("Silly Slime {}", i);
-        auto new_enemy_stats = Stats{6 + i, 8 + i, 10 + i};
+        auto new_enemy_stats = Stats{6 + (i * 2), 3 + i, 1 + i};
         auto new_enemy = std::make_unique<Enemy>(
             new_enemy_name, new_enemy_stats, new_enemy_stats.max_hp);
         new_enemy->set_position(i + 1, i + 1);
         enemies_.push_back(std::move(new_enemy));
     }
     populate_overworld();
+}
+
+PlayerCharacter& World::get_player() noexcept {
+    return player_;
 }
 
 const PlayerCharacter& World::get_player() const noexcept {
